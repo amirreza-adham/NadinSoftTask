9cd4e67b6a740c118541ac913da23d2f2286e99a
amirr <amirr@ADHAM>
2026-02-18 15:08:58 +0330

    add AppDbContext

---
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NadinSoftTask.Domain.Entities;
+
+namespace NadinSoftTask.Infrastructure.Persistence
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Product>()
+                .HasIndex(p => new { p.ProduceDate, p.ManufactureEmail })
+                .IsUnique();
+        }
+    }
+}
