a8bf97c46810d25fa64ee5765b6658cb426b0cf9
amirr <amirr@ADHAM>
2026-02-18 21:28:46 +0330

    Add UpdateProductHandler

---
+++ Application/Products/Handlers/UpdateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NadinSoftTask.Application.Products.Commands;
+using NadinSoftTask.DTOs.Products;
+using NadinSoftTask.Infrastructure.Persistence;
+
+namespace NadinSoftTask.Application.Products.Handlers
+{
+    public class UpdateProductHandler : IRequestHandler<UpdateProductCommand, ProductDto>
+    {
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+
+        public UpdateProductHandler(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductDto> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _db.Products.FindAsync(new object[] { request.Id }, cancellationToken);
+            if (product == null) throw new KeyNotFoundException("Product not found");
+
+            if (product.CreatedByUserId != request.UserId) throw new UnauthorizedAccessException();
+
+            _mapper.Map(request.Dto, product);
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return _mapper.Map<ProductDto>(product);
+        }
+    }
+
+}
