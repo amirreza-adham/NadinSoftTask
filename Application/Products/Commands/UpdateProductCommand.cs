0db916f0a0cdc401d49656ce67178ba250558185
amirr <amirr@ADHAM>
2026-02-18 21:22:19 +0330

    Add UpdateProductCommand.cs

---
+++ Application/Products/Commands/UpdateProductCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using NadinSoftTask.DTOs.Products;
+
+
+namespace NadinSoftTask.Application.Products.Commands
+{
+    public record UpdateProductCommand(int Id, CreateProductDto Dto, string UserId) : IRequest<ProductDto>;
+
+}
