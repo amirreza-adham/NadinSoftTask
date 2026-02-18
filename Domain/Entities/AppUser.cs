bc0002ae88f20c50696dff4c958665407fb28c71
amirr <amirr@ADHAM>
2026-02-18 14:48:42 +0330

    Add Entities

---
+++ Domain/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NadinSoftTask.Domain.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string Address { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NadinSoftTask.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ProduceDate { get; set; }
+
+        [Required]
+        public bool IsAvailable { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string ManufactureEmail { get; set; }
+
+        [Required]
+        [Phone]
+        [MaxLength(20)]
+        public string ManufacturePhone { get; set; }
+
+        [Required]
+        public string CreatedByUserId { get; set; } // FK  Users
+    }
+}
