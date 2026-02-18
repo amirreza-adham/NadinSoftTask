600b71773c7456137fa21fc295c197fccfe4ead7
amirr <amirr@ADHAM>
2026-02-18 19:36:08 +0330

    add create action in ProductController

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NadinSoftTask.Domain.Entities;
+using NadinSoftTask.DTOs.Products;
+using NadinSoftTask.Infrastructure.Persistence;
+using System.Security.Claims;
 
 namespace NadinSoftTask.Controllers
 {
@@ -8,6 +15,29 @@ namespace NadinSoftTask.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+
+        public ProductsController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(CreateProductDto dto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var product = _mapper.Map<Product>(dto);
+            product.CreatedByUserId = userId;
+
+            _db.Products.Add(product);
+            await _db.SaveChangesAsync();
+
+            return Ok(_mapper.Map<ProductDto>(product));
+        }
+
     }
 }
