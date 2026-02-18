0d0a9aee1d02213a5d5ff84f5c1cc187852647d1
amirr <amirr@ADHAM>
2026-02-18 21:36:37 +0330

    add GetAllProductsHandler

---
+++ Application/Products/Handlers/GetAllProductsHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NadinSoftTask.Application.Products.Queries;
+using NadinSoftTask.DTOs.Products;
+using NadinSoftTask.Infrastructure.Persistence;
+namespace NadinSoftTask.Application.Products.Handlers
+{
+    public class GetAllProductsHandler : IRequestHandler<GetAllProductsQuery, List<ProductDto>>
+    {
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+
+        public GetAllProductsHandler(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductDto>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            var query = _db.Products.AsQueryable();
+            if (!string.IsNullOrEmpty(request.CreatedByUserId))
+                query = query.Where(p => p.CreatedByUserId == request.CreatedByUserId);
+
+            var products = await query.ToListAsync(cancellationToken);
+            return _mapper.Map<List<ProductDto>>(products);
+        }
+    }
+}
+
