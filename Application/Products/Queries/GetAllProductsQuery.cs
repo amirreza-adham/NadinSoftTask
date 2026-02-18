f217966246b68bbbbd83f70027914dcab6705760
amirr <amirr@ADHAM>
2026-02-18 21:25:18 +0330

    Add GetAllProductsQuery.cs

---
+++ Application/Products/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using NadinSoftTask.DTOs.Products;
+
+namespace NadinSoftTask.Application.Products.Queries
+{
+    public record GetAllProductsQuery(string? CreatedByUserId) : IRequest<List<ProductDto>>;
+}
